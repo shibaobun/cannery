421badbb90fa6e5f3f897e33211e7a95b3da4384
shibao <shibao@bubbletea.dev>
2022-01-22 15:19:19 -0500

    add mix format to tests

--- mix.exs
+++ mix.exs
@@ -69,7 +69,12 @@ defmodule Cannery.MixProject do
       setup: ["deps.get", "compile", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: [
+        "format --check-formatted",
+        "ecto.create --quiet",
+        "ecto.migrate --quiet",
+        "test"
+      ]
     ]
   end
 end
