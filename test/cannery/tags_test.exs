c1b04bd14c1d1009d45db72fd1b16d4aa05931d8
shibao <shibao@bubbletea.dev>
2022-02-16 20:03:14 -0500

    pass tags test

--- test/cannery/tags_test.exs
+++ test/cannery/tags_test.exs
@@ -1,83 +1,75 @@
 defmodule Cannery.TagsTest do
-  use Cannery.DataCase
+  @moduledoc """
+  Tests the Tags context
+  """
 
-  alias Cannery.{AccountsFixtures, Tags}
+  use Cannery.DataCase
+  alias Cannery.{Tags, Tags.Tag}
   alias Ecto.Changeset
 
+  @moduletag :tags_test
+
+  @valid_attrs %{
+    "bg_color" => "some bg-color",
+    "name" => "some name",
+    "text_color" => "some text-color"
+  }
+  @update_attrs %{
+    "bg_color" => "some updated bg-color",
+    "name" => "some updated name",
+    "text_color" => "some updated text-color"
+  }
+  @invalid_attrs %{
+    "bg_color" => nil,
+    "name" => nil,
+    "text_color" => nil
+  }
+
   describe "tags" do
-    alias Cannery.Tags.Tag
-
-    @valid_attrs %{
-      "bg_color" => "some bg-color",
-      "name" => "some name",
-      "text_color" => "some text-color"
-    }
-    @update_attrs %{
-      "bg_color" => "some updated bg-color",
-      "name" => "some updated name",
-      "text_color" => "some updated text-color"
-    }
-    @invalid_attrs %{
-      "bg_color" => nil,
-      "name" => nil,
-      "text_color" => nil
-    }
-
-    def tag_fixture(attrs \\ %{}) do
-      %{id: user_id} = AccountsFixtures.user_fixture()
-
-      {:ok, tag} =
-        attrs
-        |> Map.put("user_id", user_id)
-        |> Enum.into(@valid_attrs)
-        |> Tags.create_tag()
-
-      tag
+    setup do
+      current_user = user_fixture()
+      [tag: tag_fixture(current_user), current_user: current_user]
     end
 
-    test "list_tags/0 returns all tags" do
-      tag = tag_fixture()
-      assert Tags.list_tags() == [tag]
+    test "list_tags/0 returns all tags", %{tag: tag, current_user: current_user} do
+      assert Tags.list_tags(current_user) == [tag]
     end
 
-    test "get_tag!/1 returns the tag with given id" do
-      tag = tag_fixture()
-      assert Tags.get_tag!(tag.id) == tag
+    test "get_tag!/1 returns the tag with given id", %{tag: tag, current_user: current_user} do
+      assert Tags.get_tag!(tag.id, current_user) == tag
     end
 
-    test "create_tag/1 with valid data creates a tag" do
-      assert {:ok, %Tag{} = tag} = Tags.create_tag(@valid_attrs)
+    test "create_tag/1 with valid data creates a tag", %{tag: tag, current_user: current_user} do
+      assert {:ok, %Tag{} = tag} = Tags.create_tag(@valid_attrs, current_user)
       assert tag.bg_color == "some bg-color"
       assert tag.name == "some name"
       assert tag.text_color == "some text-color"
     end
 
-    test "create_tag/1 with invalid data returns error changeset" do
-      assert {:error, %Changeset{}} = Tags.create_tag(@invalid_attrs)
+    test "create_tag/1 with invalid data returns error changeset",
+         %{current_user: current_user} do
+      assert {:error, %Changeset{}} = Tags.create_tag(@invalid_attrs, current_user)
     end
 
-    test "update_tag/2 with valid data updates the tag" do
-      tag = tag_fixture()
-      assert {:ok, %Tag{} = tag} = Tags.update_tag(tag, @update_attrs)
+    test "update_tag/2 with valid data updates the tag", %{tag: tag, current_user: current_user} do
+      assert {:ok, %Tag{} = tag} = Tags.update_tag(tag, @update_attrs, current_user)
       assert tag.bg_color == "some updated bg-color"
       assert tag.name == "some updated name"
       assert tag.text_color == "some updated text-color"
     end
 
-    test "update_tag/2 with invalid data returns error changeset" do
-      tag = tag_fixture()
-      assert {:error, %Changeset{}} = Tags.update_tag(tag, @invalid_attrs)
-      assert tag == Tags.get_tag!(tag.id)
+    test "update_tag/2 with invalid data returns error changeset",
+         %{tag: tag, current_user: current_user} do
+      assert {:error, %Changeset{}} = Tags.update_tag(tag, @invalid_attrs, current_user)
+      assert tag == Tags.get_tag!(tag.id, current_user)
     end
 
-    test "delete_tag/1 deletes the tag" do
-      tag = tag_fixture()
-      assert {:ok, %Tag{}} = Tags.delete_tag(tag)
-      assert_raise Ecto.NoResultsError, fn -> Tags.get_tag!(tag.id) end
+    test "delete_tag/1 deletes the tag", %{tag: tag, current_user: current_user} do
+      assert {:ok, %Tag{}} = Tags.delete_tag(tag, current_user)
+      assert_raise Ecto.NoResultsError, fn -> Tags.get_tag!(tag.id, current_user) end
     end
 
-    test "change_tag/1 returns a tag changeset" do
-      tag = tag_fixture()
+    test "change_tag/1 returns a tag changeset", %{tag: tag} do
       assert %Changeset{} = Tags.change_tag(tag)
     end
   end
