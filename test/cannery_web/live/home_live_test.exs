bbd2fd795e8766a2a7ab4c28f1eb1a4bb8cf87ad
shibao <shibao@bubbletea.dev>
2022-02-16 22:52:22 -0500

    pass home live test

--- test/cannery_web/live/home_live_test.exs
+++ test/cannery_web/live/home_live_test.exs
@@ -1,11 +1,17 @@
 defmodule CanneryWeb.HomeLiveTest do
+  @moduledoc """
+  Tests the home page
+  """
+
   use CanneryWeb.ConnCase
   import Phoenix.LiveViewTest
   import CanneryWeb.Gettext
 
+  @moduletag :home_live_test
+
   test "disconnected and connected render", %{conn: conn} do
     {:ok, home_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Cannery"
-    assert render(home_live) =~ "Welcome to Cannery"
+    assert disconnected_html =~ gettext("Welcome to %{name}", name: "Cannery")
+    assert render(home_live) =~ gettext("Welcome to %{name}", name: "Cannery")
   end
 end
