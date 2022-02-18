13c5ba12d2aaef064a793606596c5494895277eb
shibao <shibao@bubbletea.dev>
2022-02-17 20:46:17 -0500

    pass error view test

--- test/cannery_web/views/error_view_test.exs
+++ test/cannery_web/views/error_view_test.exs
@@ -1,17 +1,23 @@
 defmodule CanneryWeb.ErrorViewTest do
+  @moduledoc """
+  Tests the error view
+  """
+
   use CanneryWeb.ConnCase, async: true
   import CanneryWeb.Gettext
+
+  @moduletag :error_view_test
 
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
   test "renders 404.html" do
-    assert render_to_string(CanneryWeb.ErrorView, "404.html", []) ==
-             dgettext("errors", "Not Found")
+    assert render_to_string(CanneryWeb.ErrorView, "404.html", []) =~
+             dgettext("errors", "Not found")
   end
 
   test "renders 500.html" do
-    assert render_to_string(CanneryWeb.ErrorView, "500.html", []) ==
+    assert render_to_string(CanneryWeb.ErrorView, "500.html", []) =~
              dgettext("errors", "Internal Server Error")
   end
 end
