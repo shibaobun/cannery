726ede7e4643ba96019a2dab6c65d8735902eca8
shibao <shibao@bubbletea.dev>
2022-11-10 19:27:31 -0500

    don't run regular mix format in mix test

--- mix.exs
+++ mix.exs
@@ -86,7 +86,7 @@ defmodule Cannery.MixProject do
       setup: ["deps.get", "compile", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      format: [
+      "format.all": [
         "cmd npm run format --prefix assets",
         "format",
         "gettext.extract --merge",
