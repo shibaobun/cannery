9835fe3f5e4e0ac1ad2dbbedf56574fc56be5ad5
shibao <shibao@bubbletea.dev>
2023-03-30 22:32:53 -0400

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cannery.MixProject do
   def project do
     [
       app: :cannery,
-      version: "0.9.0",
+      version: "0.9.1",
       elixir: "1.14.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
